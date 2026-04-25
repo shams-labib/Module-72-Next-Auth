9f00acdc9244ed99e858c8123a0d686dc66f0fde
shams al labib <samslabib19@gmail.com>
2026-04-25 13:31:21 +0600

    72.2 finished

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,21 +1,41 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const userList = [
+  { name: "hablu", password: "1234" },
+  { name: "dablu", password: "5678" },
+  { name: "bablu", password: "8901" },
+];
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     CredentialsProvider({
       // Sign in with {name} button
-      name: "Credentials",
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
+      name: "Click to go",
+
       credentials: {
         username: { label: "Username", type: "text", placeholder: "jsmith" },
         password: { label: "Password", type: "password" },
+        secretCode: {
+          label: "Secret Code",
+          type: "number",
+          placeholder: "Enter code",
+        },
       },
       async authorize(credentials, req) {
+        const { username, password, secretCode } = credentials;
+
+        const user = userList.find((u) => u.name == username);
+        if (!user) {
+          return null;
+        }
+
+        const isPasswordOk = user.password == password;
+        if (isPasswordOk) {
+          return user;
+        }
+
         // My own logic is loading
 
         return null;
@@ -24,4 +44,5 @@ export const authOptions = {
   ],
 };
 
-export default NextAuth(authOptions);
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
